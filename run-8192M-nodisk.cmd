b429c14cb8e65157261dd40713cde2a4cb688ea2
you <you>
2019-04-19 04:54:47 +0800

    remove boot2docker and add download from powershell

--- run-8192M-nodisk.cmd
+++ run-8192M-nodisk.cmd
@@ -1,17 +1,3 @@
 @echo off
-set QEMU=%~dp0qemu.2019.2\
-set QEMU.EXE=%QEMU%qemu-system-x86_64.exe
-set QEMU-IMG.EXE=%QEMU%qemu-img.exe
-set DOCKER.ISO=%~dp0boot2docker.iso
-set HDA=%~dp0hda.raw
-call :run-qemu
-exit /b
-
-:run-qemu
-%QEMU.EXE% -net nic -net user,hostfwd=tcp::5022-:22 -cpu max -cdrom %DOCKER.ISO% -m 8192M -boot d %*
-exit /b
-:create-disk
-call :run-qemu
-exit /b
-
-REM -kernel bzImage -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw console=tty0"
+setlocal
+call run_all -m 8192M
