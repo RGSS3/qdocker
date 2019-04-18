ab5895b96fc89d0a9edf88b7df0325e6d4dda71d
you <you>
2019-04-19 05:47:08 +0800

    remove putty and add downloads

--- check_components.cmd
+++ check_components.cmd
@@ -11,8 +11,19 @@ if not exist "qemu.2019.2\qemu-system-x86_64.exe" (
   del qemu-setup.exe
 )
 
+if not exist "putty\putty.exe" (
+  echo Fetching SSH tool
+  call :download https://the.earth.li/~sgtatham/putty/0.71/w64/putty.zip putty.zip
+  echo Unzipping SSH tool
+  call :unzip putty.zip putty
+)
+
 exit /b
 
 :download 
 powershell -c "[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12;Invoke-WebRequest -uri %1 -OutFile %2"
+exit /b
+
+:unzip
+powershell -c "Expand-Archive -DestinationPath %2 %1"
 exit /b
